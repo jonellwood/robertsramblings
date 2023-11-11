f1323ea0ab140ab142dd9fa6ee1c754116b98185
Jon Ellwood <jonellwood@gmail.com>
2023-11-11 13:10:28 -0500

    unsure why github is doing this

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Roberts Ramblings';
-export const SITE_DESCRIPTION = 'This site is all about my Journey';
